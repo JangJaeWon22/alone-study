0775fade0102e2a81dd3d8c0e668fadd40e6fce9
JangJaeWon22 <nic2866@naver.com>
2021-10-02 21:31:10 +0900

    프론트단 login javascript 파일 생성 및 연결

--- app/app.js
+++ app/app.js
@@ -5,9 +5,11 @@ const app = express(); //모듈
 const home = require("./src/routes/home"); //라우팅
 
 //views 연결
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`))
 app.use("/", home); // use 미들웨어
+
 
 module.exports = app;
