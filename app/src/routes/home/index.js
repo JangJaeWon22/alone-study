904942264b5f552af8453f4fa473c03ac2ae26f9
jjangjae <nick2866@naver.com>
2021-10-13 22:18:31 +0900

    router 및 미들웨어 수정

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -3,17 +3,16 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./home-controller");
-const { verfyToken }= require("../middlewares/auth-middlewares");
+const { verfyToken } = require("../middlewares/auth-middlewares");
 
 //컨트롤러 연결 - controller - rendering - home의 키 값 = 루트페이지
 //컨트롤러 연결 - ontroller - rendering - login의 키 값 = 로그인 페이지
 router.get("/", controller.rendering.home);
 router.get("/login", controller.rendering.login);
-router.get("/register", controller.rendering.register)
+router.get("/register", controller.rendering.register);
 router.get("/user/me", verfyToken, (req, res) => {
-    res.send({})
-})
-
+  res.send({});
+});
 
 router.post("/login", controller.process.login);
 router.post("/register", controller.process.register);
--- app/src/routes/middlewares/auth-middlewares.js
+++ app/src/routes/middlewares/auth-middlewares.js
@@ -1,10 +1,10 @@
-"use strict"
-const jwt = require('jsonwebtoken')
+"use strict";
+const jwt = require("jsonwebtoken");
 
 module.exports.verfyToken = (req, res, next) => {
-  console.log("여기좀 지나가라;;")
+  console.log("여기좀 지나가라;;");
   const { authorization } = req.headers;
-  const [authType, authToken ] = (authorization || "").split(" ")
+  const [authType, authToken] = (authorization || "").split(" ");
   console.log(authorization);
   next();
 };
